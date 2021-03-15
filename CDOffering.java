494640f1d3a8289af7abe39f03f743487ceaad72
mematthew123 <78577112+mematthew123@users.noreply.github.com>
2021-03-15 14:42:38 -0600

    Add files via upload
    incomplete but submitting prior to deadline

---
+++ CDOffering.java
@@ -0,0 +1,45 @@
+
+package com.meritamerica.assignment3;
+
+import java.text.*;
+
+public class CDOffering 
+{
+	private int term;
+	private double interestRate;
+	
+	CDOffering(int term, double interestRate)
+	{
+		this.term = term;
+		this.interestRate = interestRate;
+	}
+	
+	int getTerm(){return term;}
+	
+	double getInterestRate(){return interestRate;}
+	
+	// ------------------------------------------------ FINISH ------------------------------------------------------------------//
+	static CDOffering readFromString(String accountHolderData) throws ParseException
+	{
+		int tempTerm = 0;
+		int tempIntRate = 0;
+		try
+		{ 
+			int comma = accountHolderData.indexOf(",");
+			tempTerm = Integer.parseInt(accountHolderData.substring(0, comma));
+			tempIntRate = Integer.parseInt(accountHolderData.substring(comma, accountHolderData.length()));
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}		
+		return new CDOffering(tempTerm, tempIntRate);
+	}
+	
+	String writeToString()
+	{
+		String tempTerm = String.valueOf(term), tempIntRate = String.valueOf(interestRate);		
+		return tempTerm + "," + tempIntRate;
+	}
+
+}
---
+++ SavingsAccount.java
@@ -0,0 +1,42 @@
+package com.meritamerica.assignment3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.text.*;
+import java.util.*;
+
+import com.meritamerica.assignment3.BankAccount;
+
+public class SavingsAccount extends BankAccount
+{
+	SavingsAccount(double openingBalance)
+	{
+		super (openingBalance, 0.001);		
+	}
+	
+	SavingsAccount(long accountNumber, double balance, double interestRate, java.util.Date accountOpenedOn)
+	{
+		super(accountNumber, balance, interestRate, accountOpenedOn);	
+	}
+	
+	 static SavingsAccount readFromString(String accountData) throws ParseException
+	{
+		long tempAcctNum = 0;
+		double tempBal = 0, tempIntRate = 0;
+		Date tempOpenDate = null;
+		String[] tempArr = accountData.split(",");
+		try
+		{
+			
+			tempAcctNum = Long.parseLong(tempArr[0]);
+			tempBal = Double.parseDouble(tempArr[1]);
+			tempIntRate = Double.parseDouble(tempArr[2]);
+			tempOpenDate = new java.util.Date(tempArr[4]);
+					
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return new SavingsAccount(tempAcctNum, tempBal, tempIntRate, tempOpenDate);
+	}
+}
